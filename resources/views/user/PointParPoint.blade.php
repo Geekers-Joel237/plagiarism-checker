26bace0791396c21211fb16c0be0b76319f0bc49
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2023-01-11 10:47:02 +0100

    Update PointParPoint.blade.php

--- resources/views/user/PointParPoint.blade.php
+++ resources/views/user/PointParPoint.blade.php
@@ -166,113 +166,75 @@ ul.exception li:before {
             </div>
         </div>
 
+
         <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h4> </h4>
-              </div>
-              <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <div class="form-group row mb-4">
-
-                            <div class="col-sm-12 col-md-12">
-                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Source</label>
-                              <textarea class="summernote" >
-                                {{$oldHtml}}
-                              </textarea>
-                              <input class="btn btn-primary" type="file">Charger le document
-                              <div class=" mt-3 ">
-                                <button class="btn  btn-warning" onclick="start_plagiat_detection(<?php $oldHtml ?>);" >
-                                    Controlez le plagiat
-                                </button>
-                              </div>
-
-                            </div>
-                          </div>
-
-                </div>
-                    <div class="col">
-                        <div class="form-group row mb-4">
-
-                            <div class="col-sm-12 col-md-12">
-                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Cible</label>
-                              <textarea class="summernote">{{$newHtml}}</textarea>
-                              <input class="btn btn-primary" type="file">Charger le document
-
-                            </div>
-                          </div>
-
-                </div>
-                {{-- {{$content}} --}}
+            @if ($message = Session::get('error'))
+            <div class="alert alert-danger">
+                <p>{{ $message }}</p>
             </div>
-        </div>
+            @endif
+            @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+            <p>{{ $message }}</p>
+            </div>
+            @endif
+             @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('media.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4> </h4>
+                        </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group row mb-4">
+
+                                    <div class="col-sm-12 col-md-12">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Source</label>
+                                    <textarea class="summernote" >
+                                        {{$oldHtml}}
+                                    </textarea>
+                                    <input class="btn btn-primary" type="file" name="fileSource">Charger le document
+                                    <div class=" mt-3 ">
+                                        <button class="btn  btn-warning" onclick="start_plagiat_detection(<?php $oldHtml ?>);" >
+                                            Controlez le plagiat
+                                        </button>
+                                    </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group row mb-4">
+                                    <div class="col-sm-12 col-md-12">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Cible</label>
+                                        <textarea class="summernote">{{$newHtml}}</textarea>
+                                        <input class="btn btn-primary" type="file" name="fileCible">Charger le document
+                                    </div>
+                                </div>
+                            </div>
+                        {{-- {{$content}} --}}
+                        </div>
+                    </div>
+                </div>
+            </form>
 
     </div>
-</div>
-</div>
+
 <?php echo $firstContent ?>
 <br><br>
 <?php var_dump($all)  ?>
-        @if ($message = Session::get('error'))
-          <div class="alert alert-danger">
-             <p>{{ $message }}</p>
-          </div>
-        @endif
-        @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-           <p>{{ $message }}</p>
-        </div>
-      @endif
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                      <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-          @endif
-          <form action="{{ route('media.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="col-12">
-              <div class="card">
-                <div class="card-header">
-                  <h4> </h4>
-                </div>
-                <div class="card-body">
-                  <div class="row">
-                      <div class="col">
-                          <div class="form-group row mb-4">
-                              <div class="col-sm-12 col-md-12">
-                                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Source</label>
-                                <textarea class="summernote"></textarea>
-                                <input class="btn btn-primary" type="file" name="fileSource">Charger le document</input>
-                                <div class=" mt-3 ">
-                                  <button class="btn  btn-warning" type="submit">Controlez le plagiat</button>
-                                </div>
 
-                              </div>
-                            </div>
-                      </div>
-                      <div class="col">
-                          <div class="form-group row mb-4">
-                              <div class="col-sm-12 col-md-12">
-                                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Cible</label>
-                                <textarea class="summernote"></textarea>
-                                <input class="btn btn-primary" type="file" name="fileCible" >Charger le document</input>
-                              </div>
-                         </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-          </form>
-
-      </div>
     </section>
 @endsection
 
